fbd24bce278750abd81f0f54814cdde6d03e41a0
Tabrez Khan <tabrez@toddleapp.com>
2020-10-21 21:53:25 +0530

    minor

--- src/libs/vmsg.js
+++ src/libs/vmsg.js
@@ -361,6 +361,7 @@ export class Recorder {
     }
     this.encNode.onaudioprocess = e => {
       if (this.isPaused) {
+        console.log("isPaused");
         return false;
       }
       const samples = e.inputBuffer.getChannelData(0);
